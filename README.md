904d9fa77826b0b1dd1dd86259f2548aeb952aa4
engtasneemmaq <tasneemmaq@yahoo.com>
2022-06-16 16:41:19 +0300

    auth-api

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # auth-api
+
+
+## ![UML Diagram ACL](./assest/UML-ACL.png)
+<br>
+
+## ![WRRC](./assest/WRRC.png)
+<br>
+
+![result](./assest/signup.png)<br>
+![result](./assest/signinn.png)<br>
+![result](./assest/secret.png)<br>
+![result](./assest/users.png)<br>
+![result](./assest/img-update.png)<br>
+![result](./assest/img-delete.png)<br>
+![result](./assest/img-create.png)<br>
+![result](./assest/img-patch.png)<br>
+
+
+
+
+
+
+
+
+
---
+++ src/middlewares/acl.js
@@ -0,0 +1,21 @@
+"use strict";
+
+module.exports =(capability)=>{
+    return(req,res,next)=>{
+    try{
+        if(req.user.actions.includes(capability)){
+            next();
+        }
+        else{
+            res.status(403).send({
+                code: 403,
+                route: req.path,
+                message: "Access Denied method is not allowed",}
+            )}
+        }
+    catch(e){
+        next("Invalid Signin")
+    }
+    }
+
+}
